74f38424847733abfa362c97cf34eb67eaea3e3b
Ilya <dert_94@mail.ru>
2020-09-20 23:12:15 +0300

    module 9

--- src/main.js
+++ src/main.js
@@ -87,11 +87,11 @@ apiWithProvider.getTrips()
 
 window.addEventListener(`load`, () => {
   navigator.serviceWorker.register(`/sw.js`)
-    .then(() => {
-      console.log(`ServiceWorker available`); // eslint-disable-line
-    }).catch(() => {
-      console.error(`ServiceWorker isn't available`); // eslint-disable-line
-    });
+  .then(() => {
+    console.log(`ServiceWorker available`); // eslint-disable-line
+  }).catch(() => {
+    console.error(`ServiceWorker isn't available`); // eslint-disable-line
+  });
 });
 
 window.addEventListener(`online`, () => {
